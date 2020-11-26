aae5001cc2ae26e6043ea963342ce13c484a91a1
LukasSchmidt96 <lukas.schmidt@informatik.hs-fulda.de>
2020-11-26 12:06:08 +0100

    Test Display

--- VierGewinnt/src/Game/Main.java
+++ VierGewinnt/src/Game/Main.java
@@ -4,8 +4,15 @@ public class Main {
 
 	//Vier Gewinnt!
 	public static void main(String[] args) {
-		
-
+		int x = 6;
+		System.out.println("\n	   Vier Gewinnt");
+		System.out.println("\n| S1 | S2 | S3 | S4 | S5 | S6 | S7 |");
+		System.out.println("------------------------------------");
+		for( int i = 1; i <= x; i++) {			
+			System.out.println("|    |    |    |    |    |    |    |");
+			System.out.println("------------------------------------");
+			
+		}
 	}
 
 }
